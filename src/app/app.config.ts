8afff0ba48d84a7cc48ddd32227ce230a6865da9
Coulibaly Abdourahmane <abdou.coulibaly@outlook.fr>
2024-11-05 09:53:34 +0100

    import: todolistReducer to store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { StoreModule } from '@ngrx/store';
+import { todoListReducer } from './todo/todol-list.reducer';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore()]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideStore( {todoList: todoListReducer})]
 };
